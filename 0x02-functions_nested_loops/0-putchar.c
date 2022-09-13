24a52ac8a07aa933d94669035413f13ff9fb779d
Kirubel <kirubelalemu119@gmail.com>
2022-09-13 02:20:28 -0700

    task 0 and readme.md added

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+/**
+ * main - Entry point
+ * Description: prints '_putchar\n'
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char str[] = "_putchar\n";
+int i = 0;
+
+while (str[i] != '\0')
+	{
+	char c = str[i];
+
+	_putchar(c);
+	i++;
+	}
+return (0);
+}
