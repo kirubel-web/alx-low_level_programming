38c8b862d24cd1fcf887a150cbfcbacb14d92153
OnajiteChristabel <111015651+OnajiteChristabel@users.noreply.github.com>
2022-09-27 15:47:01 +0100

    Kk

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,9 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stddef.h>
-
-
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -14,4 +8,3 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
-#endif
