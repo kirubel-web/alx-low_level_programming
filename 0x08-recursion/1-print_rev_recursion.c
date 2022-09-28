e86a1cad1cba49b8257c3958807e0bef1c0a80f2
OnajiteChristabel <christabelonajite2005@gmail.com>
2022-09-28 14:41:33 -0700

    h

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - rrri
+ * @s: parametyer ghj
+ */
+void_print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+	}
+}
