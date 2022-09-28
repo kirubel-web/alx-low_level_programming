0cc16ac7d64ee7d0cf24dd001ef289521eeecf5e
OnajiteChristabel <christabelonajite2005@gmail.com>
2022-09-28 13:59:20 -0700

    goo

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,18 @@
-#include "main.h"                                                                                       
+#include "main.h"
 
-/**                                                                                                     
- *
- *    _puts_recursion-prints a string followed by a new line                                             *    @s:character to be printed    
- *    Returns:returns void                                                                              
- */
+/**
+* _puts_recursion - kukjuk
+* @s: character
+* Returns: return
+*/
 
-void _puts_recursion(char*s)                                                                            
-
-{                                                                                                       	        if(*s=='\0')                                                                                    
-
-			                _putchar('\n');                                                                      
-
-		        else                                                                                             {                                                                                               
-
-						                _putchar(s[0]);                                                               
-								                _puts_recursion(s + 1);                                                                
-										        }                                                                       
-}                          
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
+}
