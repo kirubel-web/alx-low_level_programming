c676af94c05f6b69f689d4465023aa68da9a65d9
OnajiteChristabel <christabelonajite2005@gmail.com>
2022-09-29 07:07:04 -0700

    h

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,12 @@
 #include "main.h"
-
-int root (int n, int ans)
+/**
+ * root - returns natural square root
+ * @n: carrier integer
+ * @ans: iterates from 1 to n
+ * Return: always return 1 on succsess
+ * on error, -1 is returned, and errno is set appropriately
+ */
+int root(int n, int ans)
 {
 	if (ans * ans == n)
 		return (ans);
@@ -8,8 +14,14 @@ int root (int n, int ans)
 		return (-1);
 	return (root(n, ans + 1));
 }
+/**
+ *_sqrt_recursion - same function
+ *@n: carrier integer
+ *Return: always 1
+ *on error, -1 is returned, and errno is set appropriately
+ */
 
 int _sqrt_recursion(int n)
 {
-	return(root (n, 0));
+	return (root(n, 0));
 }
