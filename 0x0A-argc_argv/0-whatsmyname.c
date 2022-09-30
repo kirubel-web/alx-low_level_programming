ad813d902026722985e6c4c3b1b3530c9178b059
OnajiteChristabel <christabelonajite2005@gmail.com>
2022-09-30 12:58:51 -0700

    h

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - prints its name followed by anew line
- * @argc: is an argulment counter 
+ * @argc: is an argulment counter
  * @argv: is an argument value
- * return: always xzero
+ * Return: always 0
  */
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[argc - argc]);
 	return (0);
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,2 @@
+int _putchar(char c);
+int main(int argc, char *argv[]);
