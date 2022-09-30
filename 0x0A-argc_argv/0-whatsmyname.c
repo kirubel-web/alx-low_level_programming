2f9a810ffff707cc42fe4efc180c9e7da1d8998d
OnajiteChristabel <christabelonajite2005@gmail.com>
2022-09-30 12:38:29 -0700

    h

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints its name followed by anew line
+ * @argc: is an argulment counter 
+ * @argv: is an argument value
+ * return: always xzero
+ */
+int main(int argc,char *argv[])
+{
+	printf("%s\n", argv[argc - argc]);
+	return (0);
+}
