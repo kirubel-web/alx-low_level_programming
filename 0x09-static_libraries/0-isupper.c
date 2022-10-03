b5118133f88c0bb63728b807f8239fa3154e992a
Kirubel <kirubelalemu119@gmail.com>
2022-10-03 05:42:10 -0700

    task 0

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -3,7 +3,7 @@
 /**
  * _isupper - checks is a character is uppercase
  * @c: carrier integer variable
- * Return: 1 if true, 0 if false
+ * Return: 1 if true, 0 if false.
  */
 
 int _isupper(int c)
