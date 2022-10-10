34f14f05bc322d9b09126636e4c24aa48fb03967
Kirubel <kirubelalemu119@gmail.com>
2022-10-10 13:12:38 -0700

    task done

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,12 @@
+#ifndef PI_H
+#define PI_H
+
+/*
+ * File: 1-pi.h
+ * Desc: Header file that defines a macro named PI
+ *       as an abbreviation for the token 3.14159265359.
+ */
+
+#define PI 3.14159265359
+
+#endif
