731c0c9abc269d97a0e9f9ee76f609445e62553e
kirubel-web <kirubelalemu119@gmail.com>
2023-09-29 06:32:25 -0700

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* print_binary - This is a function that prints the binary representation of a number
+* @n: is the number to be printed in binary
+* Return: returns nothing to output
+*/
+void print_binary(unsigned long int n)
+{
+	int i, c = 0;
+	
+	unsigned long int current;
+
+	for (i = 63; i >= 0; i--)
+	{
+		current = n >> i;
+
+		if ((current & 1) == 1)
+		{
+			_putchar('1');
+			c++;
+		}
+		else if (c != 0)
+			_putchar('0');
+	}
+	if (c == 0)
+		_putchar('0');
+}
