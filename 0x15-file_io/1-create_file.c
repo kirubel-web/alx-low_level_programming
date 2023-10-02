6286c4cdc6b07902b190c0df308da5ebea609042
kirubel-web <kirubelalemu119@gmail.com>
2023-10-02 07:09:37 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,7 +5,7 @@
  * @filename: name of the file 
  * @text_content: text to write
  *
- * Return: 1 on succuss, -1 on failure
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
